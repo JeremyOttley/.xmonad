11bc461646b2bad2e7660ff1ac4ab213de41df76
Jeremy Ottley <Gazaunga@gmail.com>
2020-06-19 14:16:59 -0400

    Update experiment.xmonad.hs

--- backup/experiment.xmonad.hs
+++ backup/experiment.xmonad.hs
@@ -80,7 +80,7 @@ myKeys conf@(XConfig {XMonad.modMask = modm}) = M.fromList $
     , ((modm .|. shiftMask, xK_p     ), spawn "albert")
     
     -- launch nnn
-    , ((modm .|. xK_f     ), spawn "alacritty -e nnn")
+    , ((modm,               xK_f     ), spawn "alacritty -e nnn")
 
     -- close focused window
     , ((modm .|. shiftMask, xK_c     ), kill)
