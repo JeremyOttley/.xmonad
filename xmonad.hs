089183f83b08b54699614b7f70f6fb9fd1e4bf89
Jeremy Ottley <Gazaunga@gmail.com>
2020-06-19 18:44:42 -0400

    Update xmonad.hs

--- xmonad.hs
+++ xmonad.hs
@@ -80,7 +80,7 @@ myKeys conf@(XConfig {XMonad.modMask = modm}) = M.fromList $
     --, ((modm .|. shiftMask, xK_p     ), spawn "albert")
     
     -- launch nnn
-    , ((modm,               xK_f     ), spawn "xterm -e nnn")
+    , ((modm,               xK_f     ), spawn "xterm -e nnn $CWD")
     
     -- GridSelect
     , ((modm, xK_s), spawnSelected defaultGSConfig ["joplin", "idea", "discord", "acrobat", "gimp", "spotify", "firefox"])
